99d8f15383c927f8e255fefa0fcf7de9da5762cd
Aya Gamal <63825294+ayagamal-tech@users.noreply.github.com>
2023-10-05 16:31:07 +0300

    Create L. The Brothers.cpp

---
+++ Assuit Sheets/Newcomers/Week 1 (Data type - Conditions)/L. The Brothers.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <cstdint> 
+using namespace std;
+
+#define HeartAttack ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define oo 1e9
+#define forr(i,n) for(int i=0; i < n; i++)
+/*const int N = 100;
+int arr[N];
+*/
+int main() {
+    HeartAttack
+    
+     string f1,s1,f2,s2;
+     cin>>f1>>s1;
+     string Full1=f1+s1;
+     cin>>f2>>s2;
+     string Full2=f2+s2;
+    
+    if(s1==s2){
+        cout<<"ARE Brothers";
+    }
+  else {
+        cout<<"NOT";
+      }
+      
+    return 0;
+}      
