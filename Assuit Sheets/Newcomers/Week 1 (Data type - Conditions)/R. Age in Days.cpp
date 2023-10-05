34047db72a1ad80fca2eed17b0db1664b695e0b0
Aya Gamal <63825294+ayagamal-tech@users.noreply.github.com>
2023-10-05 16:37:42 +0300

    Create R. Age in Days.cpp

---
+++ Assuit Sheets/Newcomers/Week 1 (Data type - Conditions)/R. Age in Days.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <cstdint> 
+using namespace std;
+
+#define HeartAttack ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define oo 1e9
+#define forr(i,n) for(int i=0; i < n; i++)
+/*const int N = 100;
+int arr[N];
+*/
+int main() {
+    HeartAttack
+    
+      int N;
+      cin>>N;
+    cout<<(N/365) <<" years"<<endl;
+      N=N%365;
+    cout<<(N/30)<<" months"<<endl; 
+      N=N%30;
+    cout<<(N)<<" days"<<endl;
+      
+    return 0;
+}      
