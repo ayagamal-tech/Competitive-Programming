bf2f76e65a2398023380d82ae2897ab0ece6d6e2
Aya Gamal <63825294+ayagamal-tech@users.noreply.github.com>
2023-10-05 16:21:28 +0300

    Create V. Comparison.cpp

---
+++ Assuit Sheets/Newcomers/Week 1 (Data type - Conditions)/V. Comparison.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <cstdint> 
+using namespace std;
+
+#define HeartAttack ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define oo 1e9
+#define forr(i,n) for(int i=0; i < n; i++)
+/*const int N = 100;
+int arr[N];
+*/
+int main() {
+    HeartAttack
+    
+     ll  A,B;
+     char s;    // s='-' , '*', '+'
+      cin>>A>>s>>B;
+      if(s=='>'){
+         if(A>B)
+         cout<<"Right";
+         else
+          cout<<"Wrong";
+      }
+       else if(s=='<') {
+          if(A<B)
+         cout<<"Right";
+         else
+          cout<<"Wrong";
+       }
+       else if(s=='=') {
+         if(A==B)
+         cout<<"Right";
+         else
+          cout<<"Wrong";
+           
+       }
+  
+    return 0;
+}      
