9a1b3b50d92adf85a072db2792f90171219824d9
Aya Gamal <63825294+ayagamal-tech@users.noreply.github.com>
2023-10-05 16:35:18 +0300

    Create P. First digit ! .cpp

---
+++ Assuit Sheets/Newcomers/Week 1 (Data type - Conditions)/P. First digit ! .cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <cstdint> 
+using namespace std;
+
+#define HeartAttack ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define oo 1e9
+#define forr(i,n) for(int i=0; i < n; i++)
+/*const int N = 100;
+int arr[N];
+*/
+int main() {
+    HeartAttack
+    
+      int X,Firstdigit= (X/=10);
+      cin>>X;
+      while(X>=10){
+        X/=10;
+        if(Firstdigit<=10)
+          Firstdigit;
+     
+         }
+        if(X%2==0)
+        cout << "EVEN\n";
+        else
+            cout << "ODD\n";
+     
+      
+    return 0;
+}      
