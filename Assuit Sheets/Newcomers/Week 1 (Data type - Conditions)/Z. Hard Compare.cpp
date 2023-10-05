b4025b491949d75f40b26a62bef1ae1c7624c1a7
Aya Gamal <63825294+ayagamal-tech@users.noreply.github.com>
2023-10-05 16:45:42 +0300

    Create Z. Hard Compare.cpp

---
+++ Assuit Sheets/Newcomers/Week 1 (Data type - Conditions)/Z. Hard Compare.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <cstdint> 
+using namespace std;
+
+#define HeartAttack ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define oo 1e9
+#define forr(i,n) for(int i=0; i < n; i++)
+/*const int N = 100;
+int arr[N];
+*/
+int main() {
+    HeartAttack
+    
+    double A ,B,C ,D,t,j;  
+ 
+    cin >> A >> B >> C >> D;
+ 
+    if ( (B*log(A)) > (D*log(C)) )
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+      
+    return 0;
+}      
