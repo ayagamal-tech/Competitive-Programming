968b02198c3527938aa80ab81663fe2ab0007dee
Aya Gamal <63825294+ayagamal-tech@users.noreply.github.com>
2023-10-05 18:22:53 +0300

    Create A. 1 to N.cpp

---
+++ Assuit Sheets/Newcomers/Week 2 (Loops)/A. 1 to N.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <cstdint> 
+using namespace std;
+
+#define HeartAttack ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define oo 1e9
+#define forr(i,n) for(int i=0; i < n; i++)
+/*const int N = 100;
+int arr[N];
+*/
+int main() {
+    HeartAttack
+    
+        int n; cin>>n;
+        for(int i=1;i<=n;i++){
+            cout<<i;
+           if(i!=n)cout<<"\n";
+        }
+    
+      
+    return 0;
+}      
