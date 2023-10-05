cab655b8f5c327841d638fcee1dc9500cd7727f7
Aya Gamal <63825294+ayagamal-tech@users.noreply.github.com>
2023-10-05 16:46:29 +0300

    Create X. Two intervals.cpp

---
+++ Assuit Sheets/Newcomers/Week 1 (Data type - Conditions)/X. Two intervals.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <array>
+#include <climits>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <cstdint> 
+using namespace std;
+
+#define HeartAttack ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define oo 1e9
+#define forr(i,n) for(int i=0; i < n; i++)
+/*const int N = 100;
+int arr[N];
+*/
+int main() {
+    HeartAttack
+    
+    int l1, r1, l2, r2;
+    cin >> l1 >> r1 >> l2 >> r2;
+ 
+    int left = max(l1, l2);
+    int right = min(r1, r2);
+ 
+    if (left > right) {
+        cout << -1 << endl;
+    } else {
+        cout << left << " " << right << endl;
+    }
+      
+    return 0;
+}      
